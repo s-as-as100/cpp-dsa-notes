62cd3115e7a40fb89edd1a1ffd620e8c3008a8fb
Mohd Arif <s.arifsaifi000@gmail.com>
2025-04-25 16:14:54 +0530

    theory lesson 1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* #include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+    return 0;
+}
+*/
+// Create a program to assign value and print variables, make an int variable salary with a value of 50, 000
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int salary = 50000;
+
+    cout << "salary ";
+    cout << salary;
+
+    return 0;
+}
