530457f72ce0380aacaabf6c8dc164337fd2fca5
Mohd Arif <s.arifsaifi000@gmail.com>
2025-04-25 18:14:12 +0530

    theory practical question 2

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ int main()
 
 // Create two variables number1 and number2 with values 123 and 12451.34. Print number1 and number2 in different lines.
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -35,6 +36,47 @@ int main()
 
     cout << number1 << endl;
     cout << number2 << endl;
+
+    return 0;
+}
+
+
+// 1 .Create a program to find the area of square having side of 8.7 units.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int side = 8.7;
+    double areaOfSquare = side * side;
+
+    cout << "Area of square is " << areaOfSquare;
+
+    return 0;
+}
+
+*/
+
+// Create a program to divide N candies to M number of students.Suppose you have to divide 18 candies among 4 students equally.How many candies will each student get if candies must be divided equally ?
+
+//  And how many candies will be left that cannot be divided?
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int candies = 18;
+    int noOfStudent = 4;
+
+    int candiesCentPerStudent = candies / noOfStudent;
+    int candiesLeft = candies % noOfStudent;
+
+    cout << "Candies per student " << candiesCentPerStudent << endl;
+    cout << "Candies left " << candiesLeft << endl;
 
     return 0;
 }
