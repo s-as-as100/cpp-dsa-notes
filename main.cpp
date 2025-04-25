40f389446774e5d58598baaaef50c97c5ed05de3
Mohd Arif <s.arifsaifi000@gmail.com>
2025-04-25 16:20:12 +0530

    data types in c++ basic program

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
 }
 */
 // Create a program to assign value and print variables, make an int variable salary with a value of 50, 000
+/*
 #include <iostream>
 using namespace std;
 
@@ -17,6 +18,23 @@ int main()
 
     cout << "salary ";
     cout << salary;
+
+    return 0;
+}
+    */
+
+// Create two variables number1 and number2 with values 123 and 12451.34. Print number1 and number2 in different lines.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number1 = 123;
+    float number2 = 12451.34;
+
+    cout << number1 << endl;
+    cout << number2 << endl;
 
     return 0;
 }
