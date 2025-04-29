ece32891be9eed14d06782c5b374a3c8bd7dab52
Mohd Arif <s.arifsaifi000@gmail.com>
2025-04-29 17:10:55 +0530

    diamond pattern

--- main.cpp
+++ main.cpp
@@ -416,4 +416,52 @@ int main()
 }
 
 
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number " << endl;
+    cin >> num;
+
+    // for 1st half of diamond
+    for (int i = 1; i <= num; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    // for second half
+
+    for (int i = num - 1; i >= 1; i--)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
